5e63ef2fdb66483dcd0a21ed2212da5b071737a7
Daniel Busarello <62729531+DanielBusarello@users.noreply.github.com>
2021-12-07 23:55:05 -0300

    Added utils.java

---
+++ Semantico/src/util/utils.java
@@ -0,0 +1,71 @@
+package util;
+
+// Daniel Busarello
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class utils {
+    public static File file = null;
+
+    public static final String FLOAT = "float64";
+    public static final String INTEGER = "int64";
+    public static final String BOOL = "bool";
+    public static final String STRING = "string";
+
+    public static final String LDCR8 = "ldc.r8";
+    public static final String LDCI8 = "ldc.i8";
+    public static final String LDSTR = "ldstr";
+    public static final String CONVR8 = "conv.r8";
+    public static final String CONVI8 = "conv.i8";
+
+    public static final String ADD = "add";
+    public static final String SUB = "sub";
+    public static final String MUL = "mul";
+    public static final String DIV = "div";
+
+    public static final String AND = "AND";
+    public static final String OR = "OR";
+
+    public static boolean isInteger(String str) {
+        return str.matches("[0-9]*");
+    }
+
+    public static boolean isFloat(String str) {
+        return str.matches("[+-]?([0-9]*[.])?[0-9]+");
+    }
+
+    public static void saveFile(String text) {
+        JFileChooser fileChooser = new JFileChooser();
+
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Arquivo .txt", "txt"));
+
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+
+            if(!file.getAbsolutePath().contains(".txt")) {
+                file = new File(file.getPath() + ".txt");
+            }
+
+            saveContentFile(text);
+        }
+    }
+
+    private static void saveContentFile(String text) {
+        try {
+            PrintWriter pw = new PrintWriter(file);
+
+            pw.write(text);
+            pw.flush();
+
+            pw.close();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o arquivo. " + e.getMessage());
+        }
+    }
+}
